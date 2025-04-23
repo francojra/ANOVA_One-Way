681ac55ae76485d84318b6a3436e865a02350dd4
Jeanne Franco <44760301+francojra@users.noreply.github.com>
2025-04-22 23:30:25 -0300

    Add files via upload

--- script_ANOVA_one-way.R
+++ script_ANOVA_one-way.R
@@ -5,4 +5,15 @@
 
 # Introdução -------------------------------------------------------------------------------------------------------------------------------
 
+## O que é ANOVA One-Way?
 
+## A ANOVA (Análise de Variância) de uma via é um teste estatístico 
+## utilizado para comparar as médias de três ou mais grupos 
+## independentes, determinando se há evidências de que pelo menos 
+## uma das médias difere significativamente das demais. É uma 
+## extensão do teste t para amostras independentes.
+
+## Hipóteses:
+
+## H0: as médias de todos os grupos são iguais;
+## H1: pelo menos uma média é diferente.
